5281bd23ea2238f656fb52bb793c67824dcb8f36
Chao Hsuan Chi <allen0413@gmail.com>
2018-11-13 03:30:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,10 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each{|name| puts name}
+  array.each{|name|
+    i = 1
+    puts i + name
+    i +=1
+  }
 end
 
 def summon_captain_planet# code an argument here
